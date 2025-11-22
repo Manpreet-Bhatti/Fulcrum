ffdfa3a7a72ad9676560acbb0dec24c26833e42f
Manpreet Bhatti <manpreet@bhatti.net>
2025-11-22 17:34:01 -0500

    Implement health check

--- main.go
+++ main.go
@@ -4,35 +4,104 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type Backend struct {
 	URL          *url.URL
 	ReverseProxy *httputil.ReverseProxy
+	Alive        bool
+	mux          sync.RWMutex
+}
+
+func (backend *Backend) SetAlive(alive bool) {
+	backend.mux.Lock()
+	backend.Alive = alive
+	backend.mux.Unlock()
+}
+
+func (backend *Backend) IsAlive() bool {
+	backend.mux.RLock()
+	defer backend.mux.RUnlock()
+	return backend.Alive
 }
 
 type ServerPool struct {
-	backends []Backend
+	backends []*Backend ``
 	current  uint64
 }
 
-func (s *ServerPool) AddBackend(backend *Backend) {
-	s.backends = append(s.backends, *backend)
+func (serverPool *ServerPool) AddBackend(backend *Backend) {
+	serverPool.backends = append(serverPool.backends, backend)
 }
 
-// Return the next backend using Round Robin
-func (s *ServerPool) GetNextPeer() *Backend {
-	next := atomic.AddUint64(&s.current, 1)
+func (serverPool *ServerPool) nextIndex() int {
+	return int(atomic.AddUint64(&serverPool.current, uint64(1)) % uint64(len(serverPool.backends)))
+}
 
-	length := uint64(len(s.backends))
-	idx := next % length
+// Return the next ALIVE backend using Round Robin
+func (serverPool *ServerPool) GetNextPeer() *Backend {
+	next := serverPool.nextIndex()
+	l := len(serverPool.backends) + next
 
-	return &s.backends[idx]
+	for i := next; i < l; i++ {
+		idx := i % len(serverPool.backends)
+
+		if serverPool.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&serverPool.current, uint64(idx))
+			}
+
+			return serverPool.backends[idx]
+		}
+	}
+
+	return nil
+}
+
+func isBackendAlive(u *url.URL) bool {
+	timeout := 2 * time.Second
+	conn, err := net.DialTimeout("tcp", u.Host, timeout)
+
+	if err != nil {
+		log.Println("Site unreachable, error: ", err)
+		return false
+	}
+
+	_ = conn.Close()
+
+	return true
+}
+
+func (serverPool *ServerPool) HealthCheck() {
+	for _, backend := range serverPool.backends {
+		status := "up"
+		alive := isBackendAlive(backend.URL)
+		backend.SetAlive(alive)
+
+		if !alive {
+			status = "down"
+		}
+
+		log.Printf("%s [%s]\n", backend.URL, status)
+	}
+}
+
+func (serverPool *ServerPool) StartHealthCheck() {
+	t := time.NewTicker(time.Second * 20)
+
+	for range t.C {
+		log.Println("Starting health check...")
+		serverPool.HealthCheck()
+		log.Println("Health check completed")
+	}
 }
 
 type Config struct {
@@ -42,19 +111,23 @@ type Config struct {
 
 func LoadConfig(file string) (*Config, error) {
 	f, err := os.Open(file)
+
 	if err != nil {
 		return nil, err
 	}
+
 	defer f.Close()
 
 	var config Config
 	decoder := json.NewDecoder(f)
 	err = decoder.Decode(&config)
+
 	return &config, err
 }
 
 func main() {
 	config, err := LoadConfig("config.json")
+
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
@@ -63,24 +136,29 @@ func main() {
 
 	for _, u := range config.Backends {
 		serverURL, err := url.Parse(u)
+
 		if err != nil {
 			log.Fatalf("Invalid backend URL: %v", err)
 		}
 
 		proxy := httputil.NewSingleHostReverseProxy(serverURL)
-		originalDirector := proxy.Director
 		proxy.Director = func(req *http.Request) {
-			originalDirector(req)
-			req.Host = serverURL.Host
+			req.Header.Add("X-Forwarded-Host", req.Host)
+			req.Header.Add("X-Origin-Host", serverURL.Host)
+			req.URL.Scheme = serverURL.Scheme
+			req.URL.Host = serverURL.Host
 		}
 
 		serverPool.AddBackend(&Backend{
 			URL:          serverURL,
 			ReverseProxy: proxy,
+			Alive:        true,
 		})
 
-		fmt.Printf("✅ Configured backend: %s\n", serverURL)
+		log.Printf("✅ Configured backend: %s\n", serverURL)
 	}
+
+	go serverPool.StartHealthCheck()
 
 	server := http.Server{
 		Addr: fmt.Sprintf(":%d", config.LBPort),
@@ -88,17 +166,17 @@ func main() {
 			peer := serverPool.GetNextPeer()
 
 			if peer != nil {
-				fmt.Printf("[Fulcrum] Dispatching to %s\n", peer.URL)
 				peer.ReverseProxy.ServeHTTP(w, r)
-			} else {
-				http.Error(w, "Service not available", http.StatusServiceUnavailable)
+				return
 			}
+
+			http.Error(w, "Service not available", http.StatusServiceUnavailable)
 		}),
 	}
 
-	fmt.Printf("⚖️  Fulcrum Load Balancer starting on port %d\n", config.LBPort)
+	log.Printf("⚖️  Fulcrum Load Balancer starting on port %d\n", config.LBPort)
 
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Error starting server: %v", err)
+		log.Fatal(err)
 	}
 }
